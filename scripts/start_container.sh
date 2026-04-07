195e15999a0e756a7c66eabc0dfcc02836403837
Hans Raj Punia <login4hansraj@gmail.com>
2026-04-07 17:12:18 +0530

    added docker image path start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull hansrajpunia/sample-python-flask-service
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 hansrajpunia/sample-python-flask-service
