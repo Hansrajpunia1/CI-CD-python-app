b4f6a419166a2406c5393d25b657e76fbaf939a0
Hans Raj Punia <login4hansraj@gmail.com>
2026-04-07 18:06:39 +0530

    update (stop old container)  start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+#Stop old container 
+/usr/bin/docker stop flask-app || true 
+/usr/bin/docker rm flask-app || true 
+
 # Pull the Docker image from Docker Hub
-docker pull hansrajpunia/sample-python-flask-service
+/usr/bin/docker pull hansrajpunia/sample-python-flask-service:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 hansrajpunia/sample-python-flask-service
+/usr/bin/docker run -d -p 80:5000 --name flask-app hansrajpunia/sample-python-flask-service:latest
